c3b9077bc659f21e6fb77d15bd043a3b2a671a71
Jarrod Dyer <jarrod@gasganno.com>
2018-09-12 00:04:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
+function destructivelyAppendKitten(name) {
+    
+}
 
+function destructivelyPrependKitten(name) {
 
+}
 
+function destructivelyRemoveLastKitten() {
 
+}
 
+function destructivelyRemoveFirstKitten() {
 
+}
 
+function appendKitten(name) {
 
+}
 
+function prependKitten(name) {
 
+}
 
+function removeLastKitten() {
 
+}
 
+function removeFirstKitten() {
 
-
+}
 
 
 
