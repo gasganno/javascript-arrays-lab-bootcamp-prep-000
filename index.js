d3129a520ed4bd7c42e07ae2e6c3eb06d0d2ec14
Jarrod Dyer <jarrod@gasganno.com>
2018-09-12 00:26:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-    kittens.splice(kittens.length, 0, name)
+    var newKittens = kittens.splice(kittens.length, 0, name)
     return kittens
 }
 
